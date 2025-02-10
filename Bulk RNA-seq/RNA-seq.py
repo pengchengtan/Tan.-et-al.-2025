bf34fef17b2c65732ea2867b529d815466a66ce4
Pengcheng Tan <59388166+pengchengtan@users.noreply.github.com>
2025-02-10 13:01:32 +0800

    Update RNA-seq.py

--- Bulk RNA-seq/RNA-seq.py
+++ Bulk RNA-seq/RNA-seq.py
@@ -31,37 +31,8 @@ for x in stage1:
     data_merge[x] = np.mean(data1.iloc[:,meta1==x], axis=1)
 
 
-#Whole transcriptome heatmap
-cg = clustermap(zscore(data1, axis=1), cmap='seismic', xticklabels=data1.columns, yticklabels=[], metric='euclidean', method='ward', cbar_kws={'label': 'Normalized Gene Expression'},figsize=(3,4))
-
-#DEG analysis and violin plot
-pval = np.array([ttest_ind(data1[['U87-DMSO-1', 'U87-DMSO-2']].iloc[i], data1[['U87-O-1', 'U87-O-2']].iloc[i]) for i in range(len(data1))])
-fdr = FDR1(pval[:,1])
-fc = np.log2(data_merge['U87-O']+1)-np.log2(data_merge['U87-DMSO']+1)
-
-
-fig, ax = plt.subplots(figsize = (4,3))
-DMfilter = np.logical_and(pval[:,1]<0.05, abs(fc)>np.log2(1.5))
-ax.scatter(fc[~DMfilter], -np.log2(pval[:,1])[~DMfilter], c='grey', s=8, edgecolors='none', alpha=0.8)
-DMfilter = np.logical_and(pval[:,1]<0.05, fc>np.log2(1.5))
-ax.scatter(fc[DMfilter], -np.log2(pval[:,1])[DMfilter], c='r', s=8, edgecolors='none', alpha=0.8)
-ax.text(-4.5,2,'Up: ' + str(np.sum(DMfilter)), c='r', fontsize=12)
-DMfilter = np.logical_and(pval[:,1]<0.05, fc<-np.log2(1.5))
-ax.scatter(fc[DMfilter], -np.log2(pval[:,1])[DMfilter], c='blue', s=8, edgecolors='none', alpha=0.8)
-ax.text(-4.5,0.5,'Down: ' + str(np.sum(DMfilter)), c='blue', fontsize=12)
-ax.plot([np.log2(1.5),np.log2(1.5)],[0,20], c='red', linestyle='dashed', linewidth=0.5)
-ax.plot([-np.log2(1.5),-np.log2(1.5)],[0,20], c='red', linestyle='dashed', linewidth=0.5)
-ax.plot([-20,20],[-np.log2(0.05),-np.log2(0.05)], c='red', linestyle='dashed', linewidth=0.5)
-ax.set_xlabel('log2(FC)')
-ax.set_ylabel('-log2(p-value)')
-ax.set_xlim([-5,5])
-ax.set_title('Osimertinib v.s. DMSO')
-plt.tight_layout()
-plt.savefig('volcano.pdf', transparent=True)
-plt.close()
-
 #pathway gene expression heatmap
-genes1 = np.array(['HSPA5', 'XBP1', 'ERN1', 'EIF2AK3', 'CANX', 'DDIT3', 'ATF4', 'ATF6', 'HERPUD1', 'DNAJC3', 'PPP1R15A'])
+genes1 = np.array(['HSPA5', 'XBP1', 'ERN1', 'EIF2AK3', 'CANX', 'DDIT3', 'ATF4', 'ATF6', 'HERPUD1', 'DNAJC3', 'PPP1R15A','EDEM1', 'SEL1L'])
 genes2 = np.array(['BAX', 'TP53', 'CASP1','CASP4','CASP8', 'CASP9','CASP10', 'FAS',  'TNFRSF10B', 'CYCS', 'PMAIP1', 'HRK', 'PTEN', 'GADD45A'])
 genes3 = np.array(['MKI67', 'CCND1','CCND2','CCND3',  'CCNE2','CDK2', 'CDK6','CDK10', 'CDK15','MTOR', 'JAK2', 'E2F6','E2F8', 'ORC1'])
 genes4 = np.array(['ATR', 'RAD51','DDB1','DDB2', 'CHEK1', 'MSH2', 'PCNA', 'RPA1', 'SMC3', 'DCLRE1B'])
